fe5ca34ac391074d2211b09d3b79aa9bc3dad82c
Matthew Elwell <matthew.elwell@flagsmith.com>
2022-11-21 20:55:14 +0000

    Step 3 - Using Feature Flags

--- main.py
+++ main.py
@@ -2,7 +2,10 @@ import os
 
 from flask import Flask, jsonify
 
-from flagsmith.flagsmith import Flagsmith
+from flagsmith import Flagsmith
+
+
+SHOP_FLAG_NAME = "shop"
 
 
 app = Flask(__name__)
@@ -14,7 +17,27 @@ games = [
     {"id": 3, "title": "Stray", "price_usd": "1000"},
 ]
 
+inventory = {
+    1: 10,
+    2: 13,
+    3: 4,
+}
+
 
 @app.route("/games", methods=["GET"])
 def list_games():
     return jsonify(games)
+
+
+@app.route("/games/<int:game_id>/buy", methods=["POST"])
+def buy_game(game_id: int):
+    flags = flagsmith.get_environment_flags()
+    if not flags.is_feature_enabled(SHOP_FLAG_NAME):
+        return jsonify({"result": "error", "message": "Shop not yet available!"})
+
+    if game_id not in inventory or inventory[game_id] == 0:
+        return jsonify({"result": "error", "message": "Game not in stock!"})
+
+    inventory[game_id] -= 1
+
+    return {"result": "success", "message": "Game bought successfully."}
